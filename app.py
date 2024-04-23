97e48ab99b8aa0e3210b7814149e501ea641bc3c
jalindardhawalestw-services <167845100+jalindardhawalestw-services@users.noreply.github.com>
2024-04-23 20:22:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.llms import Ollama
